43a67016a6bcc0e3f8432ff9944116431dfceb19
Mirslv Devops <mirslv@gmail.com>
2024-12-12 19:00:41 +0100

    chore: added fake tests and YAML file with settings for tests on PR

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Create virtual environment:
+```bash
+python3 -m venv env
+```
+
+Activate virt. env.:
+```bash
+source env/bin/activate
+```
+
+Install dependencies:
+```bash
+pip install -r requirements.txt
+```
---
+++ tests/test_fake.py
@@ -0,0 +1,21 @@
+import pytest
+
+# from common.base import NotAwkwardBase
+
+class TestFake:
+
+    @pytest.mark.initial_tests
+    def test_posistive_case_1(self):
+        a = 5
+        b = 5
+        assert a == b
+
+    @pytest.mark.initial_tests
+    def test_negative_case_1(self):
+        a = 5
+        b = 7
+        assert a == b
+
+    @pytest.mark.initial_tests
+    def test_posistive_case_2(self):
+        print('Look, this test is passed')
